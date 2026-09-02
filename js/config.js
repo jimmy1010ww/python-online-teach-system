d5bb57e763ad78e9040543678e2e415732427822
jimmy1010ww <jimmy1010ww@gmail.com>
2026-09-02 14:14:22 +0800

    Add supa datebase info

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 //   SUPABASE_ANON_KEY: anon public key(這是設計上可公開的金鑰,資料由 RLS 保護)
 // 尚未填寫時,網站以「訪客模式」運作:一切可玩,進度只存在瀏覽器本機。
 
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_URL = "https://hxsakenwrqdhenkdxjfp.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_oTloH50x0u7lx2LIL1yKPA_g3iwAgSP";
